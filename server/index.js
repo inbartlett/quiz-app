3835c35f0fa9766f2d8cb50336d2e82473dad091
Mason Dunn <masondunn.dev@gmail.com>
2022-09-20 23:18:33 -0400

    Added basic frontend to test backend

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true}, () => {
-  console.log("Connected to Database")
-})
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to Database");
+});
 
-const userRoute = require("./routes/User")
-app.use("/user", userRoute)
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
 app.listen(8800, () => {
-  console.log("Server running on port 8800")
-})
+  console.log("Server running on port 8800");
+});
