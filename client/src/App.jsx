119c2d33f4d311ad6141291d68104485f06d95ba
Mason Dunn <masondunn.dev@gmail.com>
2022-09-27 16:29:39 -0400

    Reset test client for backend due to major backend changes breaking client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import LoginForm from "./components/LoginForm";
-import RegisterForm from "./components/RegisterForm";
-import Dashboard from "./pages/Dashboard";
-import Landing from "./pages/Landing";
-import { UserContext } from "./context/UserContext";
-import { useState } from "react";
-import Class from "./pages/Class";
-import Quiz from "./pages/Quiz";
-
 function App() {
-  const [user, setUser] = useState({});
-
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/class/:classId" element={<Class />} />
-        <Route path="/class/:classId/quiz/:quizId" element={<Quiz />} />
-        <Route path="*" element={"404 Not Found"} />
-      </Routes>
-    </UserContext.Provider>
-  );
+  return <div>App</div>;
 }
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,4 @@
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOMClient.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
-);
+ReactDOMClient.createRoot(document.getElementById("root")).render(<App />);
